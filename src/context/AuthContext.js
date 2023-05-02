1be31953298b4b7d2f2df1fa0a6eec20169d56d3
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-05-01 21:45:59 -0500

    added-sign-in_compo

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,1 +1,16 @@
 import * as React from "react";
+import {Auth} from 'aws-amplify'
+
+const AuthContext = React.createContext({
+    authState : 'signIn',
+    setAuthState:() => {},
+    email : "",
+    setEmail:()=>{},
+   verificationCode : "",
+    setVerificationCode:()=>{},
+   isLoading : false,
+   handleSigIn :()=>{},
+   handleSigUp :()=>{},
+   handleConfirmSigUp :()=>{},
+})
+
